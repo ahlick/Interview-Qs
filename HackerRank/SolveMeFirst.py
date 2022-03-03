05b20ccd1ee871aa3dadbe807dd8f7556baaf802
ahlick <nickchoo888@hotmail.com>
2022-03-03 18:21:45 +1100

    SolveMeFirst - Hacker Rank

---
+++ HackerRank/SolveMeFirst.py
@@ -0,0 +1,11 @@
+
+def solveMeFirst(a,b):
+    return a+b
+	# Hint: Type return a+b below
+
+
+num1 = int(input("First Number: "))
+num2 = int(input("Second Number: "))
+res = solveMeFirst(num1,num2)
+print (res)
+
